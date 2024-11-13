894ae37cae77127bc6668f373bda1ffa401fb27a
Raisul Islam <mdraju3248@gmail.com>
2024-11-13 17:27:02 +0600

    Added edit file to retrieve form data

--- edit.php
+++ edit.php
@@ -18,7 +18,18 @@ if(isset($_GET['id'])){
     }
 }
 
+if (isset($_POST['submit'])) {
+    // Retrieve form data
+    $website_name = $_POST['web_name'];
+    $position = $_POST['position'];
+    $company_name = $_POST['company_name'];
+    $salary = $_POST['salary'];
+    $status = $_POST['status'];
+    $job_link = $_POST['job_link'];
 
+   
+}
+    
 
 
 
