b5778fdd0849a9ec7dff02676d3bbca0531eb286
Raisul Islam <mdraju3248@gmail.com>
2024-11-13 17:20:17 +0600

    Added sql query to insert data into the database

--- create.php
+++ create.php
@@ -10,7 +10,18 @@ if (isset($_POST['submit'])) {
     $status = $_POST['status'];
     $job_link = $_POST['job_link'];
 
-    
+    // SQL query to insert data into the database
+    $sql = "INSERT INTO application(web_name, position, company_name, salary, status, job_link) VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt = $pdo->prepare($sql);
+    try {
+        // Execute the query with the provided values
+        $stmt->execute([$website_name, $position, $company_name, $salary, $status, $job_link]);
+        //successful submission
+        header("Location: index.php");
+        exit();
+    } catch (PDOException $e) {
+        echo "Error inserting data: " . $e->getMessage();
+    }
 } else {
     echo "No data inserted.";
 }
