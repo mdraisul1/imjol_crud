0c233ed5aba13164c1b5ee74b6c8f962a945c565
Raisul Islam <mdraju3248@gmail.com>
2024-11-13 17:29:57 +0600

    add create data edit and updata

--- edit.php
+++ edit.php
@@ -45,4 +45,52 @@ if (isset($_POST['submit'])) {
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Job Application</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/edit.css">
+</head>
+<body>
+    <div class="container-width mt-5">
+        <h4>Edit Job Application</h4>
+        <form action="edit.php?id=<?php echo $data['id']; ?>" method="POST">
+            <div class="mb-3">
+                <label for="websiteName" class="form-label">Website Name</label>
+                <input type="text" class="form-control" name="web_name" value="<?php echo $data ['web_name'] ?>">
+            </div>
+            <div class="mb-3">
+                <label for="position" class="form-label">Position</label>
+                <input type="text" class="form-control" name="position" value="<?php echo $data ['position'] ?>">
+            </div>
+            <div class="mb-3">
+                <label for="companyName" class="form-label">Company Name</label>
+                <input type="text" class="form-control" name="company_name" value="<?php echo $data ['company_name'] ?>">
+            </div>
+            <div class="mb-3">
+                <label for="salaryExpectations" class="form-label">Salary Expectations</label>
+                <input type="text" class="form-control" name="salary" value="<?php echo $data ['salary'] ?>">
+            </div>
+            <div class="mb-3">
+                <label for="status" class="form-label">Status</label>
+                <select class="form-control" name="status">
+                    <option value="Not Replied" <?php echo ($data ['status'] == 'Not Replied') ? 'selected' : ''; ?>>Not Replied</option>
+                    <option value="In Progress" <?php echo ($data ['status'] == 'In Progress') ? 'selected' : ''; ?>>In Progress</option>
+                    <option value="Replied" <?php echo ($data ['status'] == 'Replied') ? 'selected' : ''; ?>>Replied</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="jobPostLink" class="form-label">Job Post Link</label>
+                <input type="url" class="form-control" name="job_link" value="<?php echo $data ['job_link'] ?>">
+            </div>
+            <button type="submit" name="submit" class="btn btn-primary">Update</button>
+        </form>
+    </div>
+
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
 
